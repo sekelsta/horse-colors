1fd140fceb7c0e591d468fd293ddc260d26473e7
Tiffany Jager <tjager@caltech.edu>
2020-03-06 11:17:35 -0500

    re-add code removed as part of testing

--- src/main/java/horse_colors/HorseGeneticEntity.java
+++ src/main/java/horse_colors/HorseGeneticEntity.java
@@ -189,7 +189,7 @@ public class HorseGeneticEntity extends AbstractHorseEntity
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D, AbstractHorseEntity.class));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.0D));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 0.7D));
-        /*this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));*/
+        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
         this.initExtraAI();
     }
