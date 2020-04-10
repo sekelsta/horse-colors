58ac1d85ea2a6cd3dca023a3ffde6845d9be5ae4
Tiffany Jager <tjager@caltech.edu>
2020-04-09 21:38:02 -0400

    add 'highlight' blend mode

--- src/main/java/horse_colors/renderer/ComplexLayeredTexture.java
+++ src/main/java/horse_colors/renderer/ComplexLayeredTexture.java
@@ -62,6 +62,9 @@ public class ComplexLayeredTexture extends Texture {
             case SHADE:
                 shadeLayer(base, image, layer);
                 break;
+            case HIGHLIGHT:
+                highlightLayer(base, image, layer);
+                break;
         }
     }
 
@@ -98,8 +101,18 @@ public class ComplexLayeredTexture extends Texture {
         for(int i = 0; i < image.getHeight(); ++i) {
             for(int j = 0; j < image.getWidth(); ++j) {
                 int color = base.getPixelRGBA(j, i);
-                int shading = image.getPixelRGBA(j, i);
+                int shading = layer.multiply(image.getPixelRGBA(j, i));
                 base.setPixelRGBA(j, i, layer.shade(color, shading));
+            }
+        }
+    }
+
+    public void highlightLayer(NativeImage base, NativeImage image, Layer layer) {
+        for(int i = 0; i < image.getHeight(); ++i) {
+            for(int j = 0; j < image.getWidth(); ++j) {
+                int color = base.getPixelRGBA(j, i);
+                int highlight = layer.multiply(image.getPixelRGBA(j, i));
+                base.setPixelRGBA(j, i, layer.highlight(color, highlight));
             }
         }
     }
@@ -108,7 +121,9 @@ public class ComplexLayeredTexture extends Texture {
         for(int i = 0; i < image.getHeight(); ++i) {
             for(int j = 0; j < image.getWidth(); ++j) {
                 int color = base.getPixelRGBA(j, i);
-                int maskedColor = layer.mask(color, image.getPixelRGBA(j, i));
+                // Don't multiply here because that would do the wrong thing
+                int mask = image.getPixelRGBA(j, i);
+                int maskedColor = layer.mask(color, mask);
                 base.setPixelRGBA(j, i, maskedColor);
             }
         }
@@ -184,7 +199,8 @@ public class ComplexLayeredTexture extends Texture {
             NORMAL,
             NO_ALPHA,
             MASK,
-            SHADE
+            SHADE,
+            HIGHLIGHT
         }
 
         public int multiply(int color) {
@@ -217,13 +233,64 @@ public class ComplexLayeredTexture extends Texture {
             return NativeImage.getCombined(ca, (int)b, (int)g, (int)r);
         }
         
+        public int highlight(int color, int light) {
+            float r0 = NativeImage.getRed(color);
+            float g0 = NativeImage.getGreen(color);
+            float b0 = NativeImage.getBlue(color);
+            float r1 = NativeImage.getRed(light);
+            float g1 = NativeImage.getGreen(light);
+            float b1 = NativeImage.getBlue(light);
+            float a = (float)NativeImage.getAlpha(light) / 255.0F;
+            float avg = (float)(r0 + g0 + b0) / 255.0F / 3.0F;
+            a *= 0.5f + 0.5f * avg * avg;
+            float na = 1.0F - a;
+            float r = Math.max(0, Math.min(255.0F, r1 * a + r0 * na));
+            float g = Math.max(0, Math.min(255.0F, g1 * a + g0 * na));
+            float b = Math.max(0, Math.min(255.0F, b1 * a + b0 * na));
+            int ca = NativeImage.getAlpha(color);
+            return NativeImage.getCombined(ca, (int)b, (int)g, (int)r);
+        }
+
         public int mask(int color, int mask) {
             float a = NativeImage.getAlpha(color) * NativeImage.getAlpha(mask);
             a /= 255.0F;
+            float weight = this.alpha / 255f;
+            a = a * weight + NativeImage.getAlpha(color) * (1 - weight);
             int r = NativeImage.getRed(color);
             int g = NativeImage.getGreen(color);
             int b = NativeImage.getBlue(color);
             return NativeImage.getCombined((int)a, b, g, r);
+        }
+
+        // Restrict to range [0, 255]
+        public void clamp() {
+            this.red = Math.max(0, Math.min(this.red, 255));
+            this.green = Math.max(0, Math.min(this.green, 255));
+            this.blue = Math.max(0, Math.min(this.blue, 255));
+        }
+
+        private String getAbv(String s) {
+            int i = s.lastIndexOf("/");
+            if (i > -1) {
+                s = s.substring(i + 1);
+            }
+            if (s.endsWith(".png")) {
+                s = s.substring(0, s.length() - 4);
+            }
+            return s;
+        }
+
+        public String toString() {
+            if (this.name == null) {
+                return "";
+            }
+            String s = getAbv(this.name);
+            s += "-" + this.type.toString();
+            s += "-" + Integer.toHexString(this.alpha);
+            s += Integer.toHexString(this.red);
+            s += Integer.toHexString(this.green);
+            s += Integer.toHexString(this.blue);
+            return s;
         }
     }
 }
