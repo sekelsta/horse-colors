649557403a0c4621da753cd7d21f55bee29d0b01
Tiffany Jager <tjager@caltech.edu>
2020-07-18 20:13:19 -0400

    Comment out donkeys

--- src/main/java/horse_colors/entity/ModEntities.java
+++ src/main/java/horse_colors/entity/ModEntities.java
@@ -110,8 +110,8 @@ public class ModEntities {
         public static void registerSpawnEggs(RegistryEvent.Register<Item> event) {
 
             event.getRegistry().register(HORSE_SPAWN_EGG);
-            event.getRegistry().register(DONKEY_SPAWN_EGG);
-            event.getRegistry().register(MULE_SPAWN_EGG);
+            //event.getRegistry().register(DONKEY_SPAWN_EGG);
+            //event.getRegistry().register(MULE_SPAWN_EGG);
         }
 
     }
@@ -130,9 +130,9 @@ public class ModEntities {
         addSpawn(HORSE_GENETIC, horsePlainsWeight, 2, 6, BiomeDictionary.Type.PLAINS);
         int horseSavannaWeight = (int)Math.round(1 * HorseConfig.SPAWN.horseSpawnMultiplier.get());
         addSpawn(HORSE_GENETIC, horseSavannaWeight, 2, 6, BiomeDictionary.Type.SAVANNA);
-        int donkeyWeight = (int)Math.round(1 * HorseConfig.SPAWN.donkeySpawnMultiplier.get());
+        /*int donkeyWeight = (int)Math.round(1 * HorseConfig.SPAWN.donkeySpawnMultiplier.get());
         addSpawn(DONKEY_GENETIC, donkeyWeight, 1, 3, BiomeDictionary.Type.PLAINS);
-        addSpawn(DONKEY_GENETIC, donkeyWeight, 1, 1, BiomeDictionary.Type.SAVANNA);
+        addSpawn(DONKEY_GENETIC, donkeyWeight, 1, 1, BiomeDictionary.Type.SAVANNA);*/
     }
 
     private static void addSpawn(EntityType<? extends LivingEntity> entity, int weight, int minHerdSize, int maxHerdSize, BiomeDictionary.Type biometype) {
@@ -168,11 +168,11 @@ public class ModEntities {
                     if (entry.entityType == EntityType.HORSE && HorseConfig.SPAWN.blockVanillaHorseSpawns.get()) {
                         HorseColors.logger.debug("Removing vanilla horse spawn: " + entry + " from biome " + biome);
                         horseSpawns.add(entry);
-                    }
+                    }/*
                     else if (entry.entityType == EntityType.DONKEY && HorseConfig.SPAWN.blockVanillaDonkeySpawns.get()) {
                         HorseColors.logger.debug("Removing vanilla donkey spawn: " + entry + " from biome " + biome);
                         horseSpawns.add(entry);
-                    }
+                    }*/
                 }
                 for (Biome.SpawnListEntry horseSpawn : horseSpawns) {
                     spawns.remove(horseSpawn);
