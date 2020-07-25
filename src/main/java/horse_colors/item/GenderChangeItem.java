16d74ba52c78ae6b8783f099eca2f71bbe4fc802
Tiffany Jager <tjager@caltech.edu>
2020-07-25 14:31:29 -0400

    Tooltip for gender change potion

--- src/main/java/horse_colors/item/GenderChangeItem.java
+++ src/main/java/horse_colors/item/GenderChangeItem.java
@@ -1,5 +1,9 @@
 package sekelsta.horse_colors.item;
 
+import java.util.List;
+import javax.annotation.Nullable;
+
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.LivingEntity;
@@ -7,7 +11,17 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import sekelsta.horse_colors.config.HorseConfig;
 import sekelsta.horse_colors.genetics.IGeneticEntity;
+import sekelsta.horse_colors.HorseColors;
 
 public class GenderChangeItem extends Item {
     public GenderChangeItem(Item.Properties builder) {
@@ -40,6 +54,17 @@ public class GenderChangeItem extends Item {
     @Override
     public boolean hasEffect(ItemStack stack) {
         return true;
+    }
+
+    /**
+     * allows items to add custom lines of information to the mouseover description
+     */
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        if (!HorseConfig.isGenderEnabled()) {
+            String translation = HorseColors.MODID + ".gender_change_item.gender_disabled_warning";
+            tooltip.add(new TranslationTextComponent(translation).applyTextStyle(TextFormatting.GRAY));
+        }
     }
 
 }
