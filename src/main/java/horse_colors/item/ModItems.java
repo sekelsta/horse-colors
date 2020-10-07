526e65b4cbd3344d533416874efe1ff9bd405976
Tiffany Jager <tjager@caltech.edu>
2020-10-07 13:03:25 -0400

    Dispensers dispense netherite horse armor like other horse armors

--- src/main/java/horse_colors/item/ModItems.java
+++ src/main/java/horse_colors/item/ModItems.java
@@ -1,8 +1,14 @@
 package sekelsta.horse_colors.item;
 
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.*;
+import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -30,5 +36,30 @@ public class ModItems {
         netheriteHorseArmor = new CompatibleHorseArmor(13, "netherite", (new Item.Properties()).maxStackSize(1).group(CreativeTab.instance));
         netheriteHorseArmor.setRegistryName("netherite_horse_armor");
         ForgeRegistries.ITEMS.register(netheriteHorseArmor);
+        registerDispenserBehaviour();
+    }
+
+    private static void registerDispenserBehaviour() {
+        DefaultDispenseItemBehavior dispenseHorseArmor = new OptionalDispenseBehavior() {
+            /**
+             * Dispense the specified stack, play the dispense sound and spawn particles.
+             */
+            protected ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
+                BlockPos blockpos = source.getBlockPos().offset(source.getBlockState().get(DispenserBlock.FACING));
+
+                for(AbstractHorseEntity abstracthorseentity : source.getWorld().getEntitiesWithinAABB(AbstractHorseEntity.class, new AxisAlignedBB(blockpos), (horse) -> {
+                    return horse.isAlive() && horse.func_230276_fq_();
+                })) {
+                    if (abstracthorseentity.isArmor(stack) && !abstracthorseentity.func_230277_fr_() && abstracthorseentity.isTame()) {
+                        abstracthorseentity.replaceItemInInventory(401, stack.split(1));
+                        this.setSuccessful(true);
+                        return stack;
+                    }
+                }
+
+                return super.dispenseStack(source, stack);
+            }
+        };
+        DispenserBlock.registerDispenseBehavior(netheriteHorseArmor, dispenseHorseArmor);
     }
 }
