8d166b89f2a366d93a7275d4058663acc922e1b2
Tiffany Jager <tjager@caltech.edu>
2020-11-15 12:49:53 -0500

    HorseGui extends HorseInventoryScreen. Closes #11

--- src/main/java/horse_colors/client/HorseGui.java
+++ src/main/java/horse_colors/client/HorseGui.java
@@ -20,21 +20,15 @@ import sekelsta.horse_colors.entity.AbstractHorseGenetic;
 import sekelsta.horse_colors.HorseColors;
 
 @OnlyIn(Dist.CLIENT)
-public class HorseGui extends ContainerScreen<HorseInventoryContainer> {
-    private static final ResourceLocation HORSE_GUI_TEXTURES = new ResourceLocation(HorseColors.MODID, "textures/gui/horse.png");
-    /** The EntityHorse whose inventory is currently being accessed. */
-    private final AbstractHorseGenetic horseEntity;
-    /** The mouse x-position recorded during the last rendered frame. */
-    private float mousePosx;
-    /** The mouse y-position recorded during the last renderered frame. */
-    private float mousePosY;
+public class HorseGui extends HorseInventoryScreen {
+    private static final ResourceLocation TEXTURE_LOCATION = new ResourceLocation(HorseColors.MODID, "textures/gui/horse.png");
+    // The EntityHorse whose inventory is currently being accessed.
+    // This is a copy of super's horseEntity field for access without reflection.
+    private final AbstractHorseGenetic horseGenetic;
 
     public HorseGui(HorseInventoryContainer container, PlayerInventory playerInventory, AbstractHorseGenetic horse) {
-        super(container, playerInventory, horse.getDisplayName());
-        this.horseEntity = horse;
-        this.passEvents = false;/*
-        this.titleX = 8;
-        this.titleY = 6;*/
+        super(container, playerInventory, horse);
+        this.horseGenetic = horse;
     }
 
    /**
@@ -43,29 +37,28 @@ public class HorseGui extends ContainerScreen<HorseInventoryContainer> {
     @Override
     protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.getTextureManager().bindTexture(HORSE_GUI_TEXTURES);
+        this.minecraft.getTextureManager().bindTexture(TEXTURE_LOCATION);
         int i = (this.width - this.xSize) / 2;
         int j = (this.height - this.ySize) / 2;
         this.blit(matrixStack, i, j, 0, 0, this.xSize, this.ySize);
-        if (this.horseEntity instanceof AbstractChestedHorseEntity) {
-            AbstractChestedHorseEntity abstractchestedhorseentity = (AbstractChestedHorseEntity)this.horseEntity;
+        if (this.horseGenetic instanceof AbstractChestedHorseEntity) {
+            AbstractChestedHorseEntity abstractchestedhorseentity = (AbstractChestedHorseEntity)this.horseGenetic;
             if (abstractchestedhorseentity.hasChest()) {
                 this.blit(matrixStack, i + 79, j + 17, 0, this.ySize, abstractchestedhorseentity.getInventoryColumns() * 18, 54);
             }
         }
 
         // func_230264_L__() = canBeSaddled()
-        if (this.horseEntity.func_230264_L__()) {
+        if (this.horseGenetic.func_230264_L__()) {
             this.blit(matrixStack, i + 7, j + 35 - 18, 18, this.ySize + 54, 18, 18);
         }
 
         // func_230276_fq_() = wearsArmor()
-        if (this.horseEntity.func_230276_fq_()) {/*
-            if (this.horseEntity instanceof LlamaEntity) {
-                this.blit(i + 7, j + 35, 36, this.ySize + 54, 18, 18);
-            } else {*/
-                this.blit(matrixStack, i + 7, j + 35, 0, this.ySize + 54, 18, 18);
-            //}
+        if (this.horseGenetic.func_230276_fq_()) {
+            // If it were a llama would draw the carpet like this
+            // this.blit(i + 7, j + 35, 36, this.ySize + 54, 18, 18);
+            // But it's not, so draw the armor slot
+            this.blit(matrixStack, i + 7, j + 35, 0, this.ySize + 54, 18, 18);
         }
 
         if (HorseConfig.isGenderEnabled()) {
@@ -74,17 +67,17 @@ public class HorseGui extends ContainerScreen<HorseInventoryContainer> {
             int textureX = 176;
             int renderX = i + 157;
             int renderY = j + 4;
-            if (this.horseEntity.isMale()) {
+            if (this.horseGenetic.isMale()) {
                 textureX += iconWidth;
             }
             int textureY = 0;
-            if (this.horseEntity.isPregnant()) {
+            if (this.horseGenetic.isPregnant()) {
                 renderX -= 2;
                 int pregRenderX = renderX + iconWidth + 1;
                 // Blit pregnancy background
                 this.blit(matrixStack, pregRenderX, renderY + 1, 181, 23, 2, 10);
                 // Blit pregnancy foreground based on progress
-                int pregnantAmount = (int)(11 * horseEntity.getPregnancyProgress());
+                int pregnantAmount = (int)(11 * horseGenetic.getPregnancyProgress());
                 this.blit(matrixStack, pregRenderX, renderY + 11 - pregnantAmount, 177, 33 - pregnantAmount, 2, pregnantAmount);
             }
             // Blit gender icon
@@ -92,17 +85,7 @@ public class HorseGui extends ContainerScreen<HorseInventoryContainer> {
             this.blit(matrixStack, renderX, renderY, textureX, textureY, iconWidth, iconHeight);
         }
 
-        InventoryScreen.drawEntityOnScreen(i + 51, j + 60, 17, (float)(i + 51) - this.mousePosx, (float)(j + 75 - 50) - this.mousePosY, this.horseEntity);
-    }
-
-    @Override
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(matrixStack);
-        this.mousePosx = (float)mouseX;
-        this.mousePosY = (float)mouseY;
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
-        // func_230459_a_ = renderHoveredToolTip
-        this.func_230459_a_(matrixStack, mouseX, mouseY);
+        InventoryScreen.drawEntityOnScreen(i + 51, j + 60, 17, (float)(i + 51) - mouseX, (float)(j + 75 - 50) - mouseY, this.horseGenetic);
     }
 
     public static void replaceGui(GuiOpenEvent event) {
@@ -110,11 +93,11 @@ public class HorseGui extends ContainerScreen<HorseInventoryContainer> {
             HorseInventoryScreen screen = (HorseInventoryScreen)event.getGui();
             AbstractHorseEntity horse = null;
             try {
-                // field_147034_x = horseEntity
+                // field_147034_x = horseGenetic
                 horse = ObfuscationReflectionHelper.getPrivateValue(HorseInventoryScreen.class, screen, "field_147034_x");
             }
             catch (ObfuscationReflectionHelper.UnableToAccessFieldException e) {
-                System.err.println("Unable to access private value horseEntity while replacing the horse GUI.");
+                System.err.println("Unable to access private value horseGenetic while replacing the horse GUI.");
                 System.err.println(e);
             }
             if (horse instanceof AbstractHorseGenetic) {
